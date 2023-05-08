fede1e08bf81d3eb2b0c2676d2486824ae56bf5e
JamieB133 <2127147b@student.gla.ac.uk>
2023-05-08 22:02:37 +0100

    Update cmake to expect arm-gcc toolchain in the PATH.

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -3,16 +3,14 @@ cmake_minimum_required(VERSION 3.16.0)
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-set(COMPILER_ROOT ${ARM_TOOLCHAIN_DIR}/bin)
 set(TOOLCHAIN_PREFIX arm-none-eabi-)
-
-set(CMAKE_C_COMPILER ${COMPILER_ROOT}/${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_ASM_COMPILER ${COMPILER_ROOT}/${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_CXX_COMPILER ${COMPILER_ROOT}/${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 
 set(CMAKE_C_FLAGS "-mcpu=cortex-m0 -mthumb -fdata-sections -ffunction-sections -Wall -DDEBUG -O0 -g3" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS "-mcpu=cortex-m0 -mthumb -fdata-sections -ffunction-sections -Wall -DDEBUG -O0 -g3" CACHE INTERNAL "")
-set(CMAKE_EXE_LINKER_FLAGS "-T${CMAKE_SOURCE_DIR}/cubemx/STM32F051R8Tx_FLASH.ld -mcpu=cortex-m0 -mthumb -specs=nano.specs -lc -lm -lnosys -Wl,-Map=${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.map,--cref -Wl,--gc-sections -DDEBUG -O0 -g3" CACHE INTERNAL "")
+set(CMAKE_EXE_LINKER_FLAGS "-T${CMAKE_SOURCE_DIR}/STM32F051R8Tx_FLASH.ld -mcpu=cortex-m0 -mthumb -specs=nano.specs -lc -lm -lnosys -Wl,-Map=${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.map,--cref -Wl,--gc-sections -DDEBUG -O0 -g3" CACHE INTERNAL "")
 
 set(CMAKE_OBJCOPY ${COMPILER_ROOT}/${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
 set(CMAKE_SIZE_UTIL ${COMPILER_ROOT}/${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
