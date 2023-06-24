c584c5ac26af48b87b39f2b97b94e56bb3ab7818
Jamie Brown <jbrown45@jaguarlandrover.com>
2023-06-24 12:49:38 +0100

    Use the PLL as system clock source to provide 48MHz to the timer.

--- main.cpp
+++ main.cpp
@@ -37,23 +37,17 @@ static void initSystick()
 static void configureClock()
 {
     // Enable the internal  clock (HSI) and clock security system (CSS).
-    RCC->CR |= RCC_CR_CSSON | RCC_CR_HSION; 
+    RCC->CR |= RCC_CR_CSSON | RCC_CR_HSION;
 
     // Wait for HSI ready bit.
     while(!(RCC->CR & RCC_CR_HSIRDY));
-
-    // Enable 14Mhz internal clock (HSI14).
-    RCC->CR2 |= RCC_CR2_HSI14ON;
-
-    // Wait for the HSI14 ready bit.
-    while(!(RCC->CR2 & RCC_CR2_HSI14RDY));
 
     // Enable power on the clock.
     RCC->APB1ENR |= RCC_APB1ENR_PWREN;
 
     // Set the highest APB divider in order to ensure that we do not go through
     // a non-spec phase whatever we decrease or increase HCLK. 
-    MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE, RCC_CFGR_PPRE_DIV16);
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE, RCC_CFGR_PPRE_DIV1);
 
     // Set prescalar (RCC SysClock not divided).
     MODIFY_REG(RCC->CFGR, RCC_CFGR_HPRE, RCC_CFGR_HPRE_DIV1);
@@ -61,8 +55,57 @@ static void configureClock()
     // Wait for HSI ready bit.
     while(!(RCC->CR & RCC_CR_HSIRDY));
 
-    // Configure the system clock source as HSI.
+    // Configure the system clock source as HSI before we change it to the PLL.
     MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_HSI);
+
+    // Wait for the system clock to switch to HSI.
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI); 
+
+    // Disable the PLL.
+    RCC->CR &=  (uint32_t)(~RCC_CR_PLLON);
+    while((RCC->CR & RCC_CR_PLLRDY) != 0);
+
+    // Set PLL mutliplier to 12.
+    RCC->CFGR = RCC->CFGR & (~RCC_CFGR_PLLMUL) | (RCC_CFGR_PLLMUL12);
+
+    // Enable the PLL.
+    RCC->CR |= RCC_CR_PLLON;
+    while((RCC->CR & RCC_CR_PLLRDY) == 0); 
+
+    // Set the system clock as the PLL.
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_PLL);
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL); // 10
+}
+
+
+static void delay(uint32_t d)
+{
+    for(int i = 0; i < 1000; i++)
+    {
+        TIM6->CNT = 0x0000U;
+        while(TIM6->CNT < d);
+    }
+}
+
+static void initTimer()
+{
+    // Enable timer clock.
+    // From page 125 of ref manual:
+        //  Bit 4 TIM6EN: TIM6 timer clock enable
+        //  Set and cleared by software.
+        //  0: TIM6 clock disabled
+        //  1: TIM6 clock enabled
+    RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+
+    // Set prescalar.
+    TIM6->PSC = 48 - 1; // 48Mhz/48 = 1Mhz ~~ 1us delay
+    TIM6->ARR = 0xffff;
+
+    // Enable the timer.
+    TIM6->CR1 |= 0x00001U;
+
+    // Wait for update flag.
+    while(!(TIM6->SR & 0x1U));
 }
 
 static void initGPIO()
@@ -84,36 +127,6 @@ static void initGPIO()
     temp &= ~(GPIO_MODER_MODER0 << (position * 2u));
     temp |= (pushPullModeMask << (position * 2u));
     GPIOC->MODER = temp;
-}
-
-static void delay(uint32_t d)
-{
-    for(int i = 0; i < 100; i++)
-    {
-        TIM6->CNT = 0x0000U;
-        while(TIM6->CNT < d);
-    }
-}
-
-static void initTimer()
-{
-    // Enable timer clock.
-    // From page 125 of ref manual:
-        //  Bit 4 TIM6EN: TIM6 timer clock enable
-        //  Set and cleared by software.
-        //  0: TIM6 clock disabled
-        //  1: TIM6 clock enabled
-    RCC->APB1ENR |= 1 << 4;
-
-    // Set prescalar.
-    TIM6->PSC = 6;
-    TIM6->ARR = 0xffff;
-
-    // Enable the timer.
-    TIM6->CR1 |= 0x00001U;
-
-    // Wait for update flag.
-    while(!(TIM6->SR & 0x1U));
 }
 
 void SystemInit()
